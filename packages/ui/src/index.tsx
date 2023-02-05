aa236769f7e54a3cdd1efb924a6c6063d581d32e
Marcus Wood <marcuswood230@gmail.com>
2023-02-05 14:37:22 -0700

    show a repro of bug

---
+++ packages/ui/src/index.tsx
@@ -0,0 +1,8 @@
+export * from 'tamagui'
+export * from './MyComponent'
+export { config } from './tamagui.config'
+// import { LinearGradient } from 'tamagui/linear-gradient'
+
+// export const SomeComponent = () => {
+//   return <LinearGradient colors={['red', 'purple']} height={100} />
+// }
